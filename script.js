2b4015f4c1b5568567d401d4f3d6cc0c0e99f924
Matiash Oleh <matiasholeh@gmail.com>
2023-12-12 21:26:16 +0200

    use ifelse

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function greet() {
-    var d = new Date();
-    alert(`Зараз в Кієві  ${d}`);
-};
-
-
+let temperature = 16;
+let isWindy = false;
+if(temperature < 15) {
+    console.log("Вдягніться тепліше і візьміть шапку")
+} else if(!isWindy) {
+    console.log("Хороший день для прогулянки");
+}
