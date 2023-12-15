551bc047f61f3a33ef5b94495720e1f6d896e764
Matiash Oleh <matiasholeh@gmail.com>
2023-12-15 13:39:11 +0200

    for in objekt and slice

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ for(i=0; i<10; i++) {
     console.log("Виводимо " + n);
     n+=1;
 }
+
+let phone = {
+    brand: "Iphone",
+    color: "black",
+    wight: 123
+}
+
+for (let key in phone) {
+    console.log(key + ': ' + phone[key]);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+
+let newNumbers = [... numbers.slice(0, 2), 6, 7, ...numbers.slice(2, 5)];
+console.log(newNumbers);
